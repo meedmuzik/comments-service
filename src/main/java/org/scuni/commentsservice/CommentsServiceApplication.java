d178e0d1253c97ef5e6989f21e96b11d45d2c487
Semenov Egor <brose2004@mail.ru>
2024-08-17 14:38:57 +0300

    init comments-service

---
+++ src/main/java/org/scuni/commentsservice/CommentsServiceApplication.java
@@ -0,0 +1,13 @@
+package org.scuni.commentsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommentsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommentsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/scuni/commentsservice/CommentsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.scuni.commentsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommentsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
